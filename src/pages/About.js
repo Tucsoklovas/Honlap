f1041fde2898bd311cf68e2c235beb82b09bccfd
Dori <contentdori@gmail.com>
2021-04-01 19:59:28 +0200

    Minor fixes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {graphql} from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Image from "gatsby-image"
 import * as styles from "../css/about.module.css"
 
